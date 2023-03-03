3fc731c02e1f353a7a8e1aad62da522f207de54a
Archer <sp38013@naver.com>
2023-03-03 18:35:20 +0900

    JsonParser Csv 변환 및 다운로드 기능 추가  [sy.namgoong]
    1. CSV 변환 및 다운로드 기능 추가작업 완료

--- feature/jsonParser.js
+++ feature/jsonParser.js
@@ -2,13 +2,14 @@ const btnParser = document.querySelector('.parser');
 const btnClear = document.querySelector('.clear');
 const btnJsonToCsvFile = document.querySelector('.jsonToCsvFile');
 const btnJsonToXlsFile = document.querySelector('.jsonToXlsFile');
-
 const textArea = document.getElementById('jp_TextArea');
+
 let jsonDataBackup ;
-//const fs = require('fs'); //node fs 모듈 사용
 
 
-//Ex
+
+
+//Ex 예시 데이터 
 //[{"id":1,"first_name":"Eolanda","last_name":"Druce","email":"edruce0@cbsnews.com","gender":"Female"}]
 
 /*
@@ -36,10 +37,12 @@ btnParser.addEventListener('click',() => {
         let jsonData =JSON.parse(textArea.value); //json Object 로 변환
         let jsonDataParse =JSON.stringify(jsonData,null,4); //json Object 로 변환된 값을 문자열로 변환 및 탭 처리
         textArea.value =jsonDataParse ;
-        jsonDataBackup = jsonData;
-        btnJsonToCsvFile.setAttribute()
+        jsonDataBackup = jsonData; //파일 변환을 위한 json 데이터 백업
+
+        btnJsonToCsvFile.removeAttribute("disabled");
     }catch (err){
         let errMsg =textArea.value == ""? "값을 입력해주세요.":"Json형식의 데이터가 아닙니다. 다시 입력해주세요.";
+        console.log("[ERROR]  ",err);
         alert("[ERROR]" + errMsg);
         textArea.value ="";
         textArea.focus();
@@ -54,22 +57,32 @@ btnClear.addEventListener('click',() =>{
 
 //btn jsonToCsvFile
 btnJsonToCsvFile.addEventListener('click',() => {
-    let test = jsonToCSV(jsonDataBackup);
-    console.log("test => ", test);
+
+    let csvData = jsonToCSV(jsonDataBackup); //jsonData Csv 형식 데이터로 변환
+    createDownLoadLink(csvData); //변환된 데이터 다운로드
 });
 
 
 
+//임시 다운로드 링크 생성 및 클릭 이벤트
+function  createDownLoadLink(data){
+    let fileDown ="data:text;charset=utf-8,\ufeff" + data;
+    let encodedUri = encodeURI(fileDown);
+    let link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "numbers.csv");
 
+    document.body.appendChild(link); //임시로 만든 링크를 append
+    link.click(); //링크 클릭 이벤트
+    document.body.removeChild(link); //클릭 이벤트가 종료되면 삭제 처리
+}
 
-
-
-
-
-
+//clear
 function clear(){
     textArea.value = "";
     jsonDataBackup = "";
+    btnJsonToCsvFile.setAttribute("disabled",true);
+    btnJsonToXlsFile.setAttribute("disabled",true);
 }
 
 
@@ -77,21 +90,14 @@ function clear(){
 function jsonToCSV(jsonData) {
     // 1. json 데이터 취득
     const json_array = jsonData;
-    console.log(json_array);
     // 2. CSV 문자열 변수 선언: json을 csv로 변환한 문자열이 담길 변수
     let csv_string = '';
-
-
     // 3. 제목 추출: json_array의 첫번째 요소(객체)에서 제목(머릿글)으로 사용할 키값을 추출
     const titles = Object.keys(json_array[0]);
-
-
     // 4. CSV문자열에 제목 삽입: 각 제목은 컴마로 구분, 마지막 제목은 줄바꿈 추가
     titles.forEach((title, index)=>{
         csv_string += (index !== titles.length-1 ? `${title},` : `${title}\r\n`);
     });
-
-
     // 5. 내용 추출: json_array의 모든 요소를 순회하며 '내용' 추출
     json_array.forEach((content, index)=>{
 
@@ -105,14 +111,12 @@ function jsonToCSV(jsonData) {
         // CSV 문자열에 '내용' 행 삽입: 뒤에 줄바꿈(\r\n) 추가, 마지막 행은 줄바꿈X
         csv_string += (index !== json_array.length-1 ? `${row}\r\n`: `${row}`);
     })
-
     // 6. CSV 문자열 반환: 최종 결과물(string)
     return csv_string;
 }
 
 
 
-//
 
 
 
